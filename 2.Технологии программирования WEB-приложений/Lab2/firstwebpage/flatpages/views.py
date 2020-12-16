cfd8edf09576fc5f4cb2e4ad3ccfda046840433e
kernal1988 <kernal1988@gmail.com>
2020-12-16 15:49:16 +0300

    first commit

---
+++ 2.Технологии программирования WEB-приложений/Lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
+
+
+# Create your views here.
+def home(request):
+	return render(request, 'templates/static_handler.html')
